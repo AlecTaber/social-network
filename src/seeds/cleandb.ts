ffebe506f7597a1d4ce42d7fff66c533ddde53a5
Alec Taber <alectaber12@gmail.com>
2024-10-06 15:01:28 -0700

    create cleandb

---
+++ src/seeds/cleandb.ts
@@ -0,0 +1,17 @@
+import User from '../models/User.js';
+import Thought from '../models/Thought.js';
+import db from '../config/connection.js';
+
+export const cleandb = async () => {
+    try {
+        await db();
+        console.log('Clearing database...');
+        await User.deleteMany({});
+        await Thought.deleteMany({});
+        console.log('Database cleared!');
+    } catch (err) {
+        console.error(err);
+     throw new Error('Failed to clear database!');
+    }
+};
+
